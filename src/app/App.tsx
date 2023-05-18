9970730999abc3e0af57afa9dc80b6a6e33b418f
Michaverkh <michaverkh@gmail.com>
2023-05-18 10:12:59 +0300

    Перемещения файлов

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { LogInForm } from "../Login";
 import { MainPage } from "pages/main";
 
 function App() {
   return (
     <>
       <div>My Clinic</div>
-      <LogInForm />
       <MainPage />
     </>
   );
