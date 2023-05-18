994f68edf3407d5c4995ff3d017134d8580b94a3
Michaverkh <michaverkh@gmail.com>
2023-05-18 10:42:05 +0300

    Добавил роутинг в приложение

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import { MainPage } from "pages/main";
+import { LoginPage } from "pages/login";
+import { RoterPath } from "shared/router/enums";
 
 function App() {
   return (
-    <>
-      <div>My Clinic</div>
-      <MainPage />
-    </>
+    <BrowserRouter>
+      <ul>
+        <Link to={RoterPath.MAIN}>Главная</Link>
+        <Link to={RoterPath.LOGIN}>Войти</Link>
+      </ul>
+      <Routes>
+        <Route path={RoterPath.MAIN} element={<MainPage />} />
+        <Route path={RoterPath.LOGIN} element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
