d5db8bd750d5ee103405728dd98851180d14a026
Michaverkh <michaverkh@gmail.com>
2023-05-26 17:23:24 +0300

    Доработки по дашборду

---
+++ src/entities/dashboard/interafces.ts
@@ -0,0 +1,22 @@
+export interface IDashBoardData {
+  id: string;
+  name: string;
+  standart: number;
+  poorQuality: number;
+  suboptimal: number;
+  unverifiable: number;
+  date?: string;
+}
+
+export interface IDashBoardRenderData {
+  name: string;
+  "соответствует стандарту": number;
+  "не соответствует качество": number;
+  "не оптимальное": number;
+  "диагноза нет в базе": number;
+  date?: string;
+}
+
+export interface IDashBoardDataItems {
+  items: IDashBoardData[];
+}
--- src/shared/enums/enums.ts
+++ src/shared/enums/enums.ts
@@ -4,9 +4,22 @@ export enum ESpecialization {
   NEUROLOGY = "neurology",
 }
 
+export enum ESpecializationRu {
+  otolaringology = "отоларингология",
+  cardiology = "кардиология",
+  neurology = "неврология",
+}
+
 export enum EReception {
   STANDART = "standart",
   POOR_QUALITY = "poorQuality",
   SUBOPTIMAL = "suboptimal",
   UNVERIFIABLE = "unverifiable",
+}
+
+export enum EReceptionRu {
+  standart = "соответствует стандарту",
+  poorQuality = "не соответствует качество",
+  suboptimal = "не оптимальное",
+  unverifiable = "диагноза нет в базе",
 }
