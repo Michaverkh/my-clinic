6034ab0b1a56603c25609670bef78e7fbe2d237b
BorisBogatov <boris95@yandex.ru>
2023-05-27 22:57:24 +0500

    экспорт файла в лоб как в лквк

--- src/features/ExportBtn/index.tsx
+++ src/features/ExportBtn/index.tsx
@@ -1,11 +1,21 @@
-import { Button } from "@mui/material";
+import { Button, Link } from "@mui/material";
 import { FC } from "react";
 import SaveAltIcon from "@mui/icons-material/SaveAlt";
+import { EEndpoints } from "shared/api/enums";
 
 export const ExportBtn: FC = () => {
+  const timeStamp = new Date().getTime();
   return (
-    <Button color="secondary" startIcon={<SaveAltIcon />}>
-      Экспортировать
-    </Button>
+    <Link
+      href={EEndpoints.GET_REPORT_EXPORT_FILE}
+      target="_blank"
+      rel="noreferrer"
+      underline="none"
+      download={`${timeStamp}.csv`}
+    >
+      <Button color="secondary" startIcon={<SaveAltIcon />}>
+        Экспортировать
+      </Button>
+    </Link>
   );
 };
